cd999b94389eb00ccc556fb9e605b01480ccd5e1
Taylor Radtke <taylorradtke@gmail.com>
2025-01-01 14:15:37 -0600

    Updates

---
+++ precache-manifest.dd95effc3f3aaef52a77103c65a09900.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ef156c21dfaea6007122ede0e03d170",
+    "url": "/react-espn/index.html"
+  },
+  {
+    "revision": "e0f61f4af6c3bbab81a5",
+    "url": "/react-espn/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "7c40242417938c48d76e",
+    "url": "/react-espn/static/css/main.4cbd0704.chunk.css"
+  },
+  {
+    "revision": "e0f61f4af6c3bbab81a5",
+    "url": "/react-espn/static/js/2.ea22968b.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/react-espn/static/js/2.ea22968b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c40242417938c48d76e",
+    "url": "/react-espn/static/js/main.85224a1c.chunk.js"
+  },
+  {
+    "revision": "d61841d45611774bb1dc",
+    "url": "/react-espn/static/js/runtime-main.2c4e79ce.js"
+  }
+]);
